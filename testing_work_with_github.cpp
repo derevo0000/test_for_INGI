e3365550511f5e9770a7972d3b4ec563166793c2
User <User@WIN-6S15UG2SHFV>
2024-05-07 22:59:55 +0300

    Добавьте файлы проекта.

---
+++ testing_work_with_github.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string name;
+    std::cout << "Enter your name\n";
+    std::cin >> name;
+    std::cout << "Hello, " << name << "!";
+}
